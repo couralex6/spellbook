a1fbd1f3397d43140073c50661269e2fe5e72d21
Meghan <msheintz@gmail.com>
2023-03-22 14:50:34 -0400

    wip

---
+++ macros/dune/adapters.sql
@@ -0,0 +1,293 @@
+
+-- - get_catalog
+-- - list_relations_without_caching
+-- - get_columns_in_relation
+
+{% macro trino__get_columns_in_relation(relation) -%}
+  {% do log('trino__get_columns_in_relation', info=true) %}
+  {%- set sql -%}
+    describe {{ relation }}
+  {%- endset -%}
+  {%- set result = run_query(sql) -%}
+
+  {% set maximum = 10000 %}
+  {% if (result | length) >= maximum %}
+    {% set msg %}
+      Too many columns in relation {{ relation }}! dbt can only get
+      information about relations with fewer than {{ maximum }} columns.
+    {% endset %}
+    {% do exceptions.raise_compiler_error(msg) %}
+  {% endif %}
+
+  {% set columns = [] %}
+  {% for row in result %}
+    {% do columns.append(api.Column.from_description(row['Column'].lower(), row['Type'])) %}
+  {% endfor %}
+  {% do return(columns) %}
+{% endmacro %}
+
+
+{% macro trino__list_relations_without_caching(relation) %}
+  {% do log('trino__list_relations_without_caching', info=true) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      t.table_catalog as database,
+      t.table_name as name,
+      t.table_schema as schema,
+      case when mv.name is not null then 'materializedview'
+           when t.table_type = 'BASE TABLE' then 'table'
+           when t.table_type = 'VIEW' then 'view'
+           else t.table_type
+      end as table_type
+    from {{ relation.information_schema() }}.tables t
+    left join system.metadata.materialized_views mv
+          on mv.catalog_name = t.table_catalog and mv.schema_name = t.table_schema and mv.name = t.table_name
+          and (mv.catalog_name is null or mv.catalog_name =  '{{ relation.database | lower }}')
+          and (mv.schema_name is null or mv.schema_name =  '{{ relation.schema | lower }}')
+    where t.table_schema = '{{ relation.schema | lower }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
+
+
+{% macro trino__reset_csv_table(model, full_refresh, old_relation, agate_table) %}
+    {% do log('trino__reset_csv_table', info=true) %}
+    {{ adapter.drop_relation(old_relation) }}
+    {{ return(create_csv_table(model, agate_table)) }}
+{% endmacro %}
+
+
+{% macro trino__create_csv_table(model, agate_table) %}
+  {% do log('trino__create_csv_table', info=true) %}
+  {%- set column_override = model['config'].get('column_types', {}) -%}
+  {%- set quote_seed_column = model['config'].get('quote_columns', None) -%}
+  {%- set _properties = config.get('properties') -%}
+
+  {% set sql %}
+    create table {{ this.render() }} (
+        {%- for col_name in agate_table.column_names -%}
+            {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+            {%- set type = column_override.get(col_name, inferred_type) -%}
+            {%- set column_name = (col_name | string) -%}
+            {{ adapter.quote_seed_column(column_name, quote_seed_column) }} {{ type }} {%- if not loop.last -%}, {%- endif -%}
+        {%- endfor -%}
+    ) {{ properties(_properties) }}
+  {% endset %}
+
+  {% call statement('_') -%}
+    {{ sql }}
+  {%- endcall %}
+
+  {{ return(sql) }}
+{% endmacro %}
+
+{% macro properties(properties, relation) %}
+  {% do log(properties, info=true) %}
+  {% do log(relation, info=true) %}
+  {%- if properties is not none -%}
+      WITH (
+          location = 's3a://trino-dev-datasets-118330671040/hive/{{relation.schema}}/{{relation.identifier}}'
+          {%- for key, value in properties.items() -%}
+            {{ key }} = {{ value }}
+            {%- if not loop.last -%}{{ ',\n  ' }}{%- endif -%}
+          {%- endfor -%}
+      )
+  {%- else -%}
+      WITH (
+          location = 's3a://trino-dev-datasets-118330671040/hive/{{relation.schema}}/{{relation.identifier}}'
+      )
+  {%- endif -%}
+{%- endmacro -%}
+
+
+{% macro trino__create_table_as(temporary, relation, sql) -%}
+  {% do log('trino__create_table_as', info=true) %}
+  {%- set _properties = config.get('properties') -%}
+  create table {{ relation }}
+    {{ properties(_properties, relation) }}
+  as (
+    {{ sql }}
+  );
+{% endmacro %}
+
+
+{% macro trino__create_view_as(relation, sql) -%}
+  {% do log('trino__create_view_as', info=true) %}
+  {%- set view_security = config.get('view_security', 'definer') -%}
+  {%- if view_security not in ['definer', 'invoker'] -%}
+      {%- set log_message = 'Invalid value for view_security (%s) specified. Setting default value (%s).' % (view_security, 'definer') -%}
+      {% do log(log_message) %}
+      {%- set on_table_exists = 'definer' -%}
+  {% endif %}
+  create or replace view
+    {{ relation }}
+  security {{ view_security }}
+  as
+    {{ sql }}
+  ;
+{% endmacro %}
+
+
+{% macro trino__drop_relation(relation) -%}
+  {% do log('trino__drop_relation', info=true) %}
+  {% set relation_type = 'materialized view' if relation.type == 'materializedview' else relation.type %}
+  {% call statement('drop_relation', auto_begin=False) -%}
+    drop {{ relation_type }} if exists {{ relation }}
+  {%- endcall %}
+{% endmacro %}
+
+
+{# see this issue: https://github.com/dbt-labs/dbt/issues/2267 #}
+{% macro trino__information_schema_name(database) -%}
+  {%- if database -%}
+    {{ database }}.INFORMATION_SCHEMA
+  {%- else -%}
+    INFORMATION_SCHEMA
+  {%- endif -%}
+{%- endmacro %}
+
+
+{% macro create_schema(relation) -%}
+  {{ adapter.dispatch('create_schema', 'dbt')(relation) }}
+{% endmacro %}
+
+{% macro default__create_schema(relation) -%}
+  {% do log('default__create_schema', info=true) %}
+  {%- call statement('create_schema') -%}
+   CREATE SCHEMA {{ relation }} WITH (location = 's3a://trino-dev-datasets-118330671040/hive')
+  {% endcall %}
+  {% do log('end', info=true) %}
+{% endmacro %}
+
+{# On Trino, 'cascade' is not supported so we have to manually cascade. #}
+{% macro trino__drop_schema(relation) -%}
+  {% do log('trino__drop_schema', info=true) %}
+  {% for row in list_relations_without_caching(relation) %}
+    {% set rel_db = row[0] %}
+    {% set rel_identifier = row[1] %}
+    {% set rel_schema = row[2] %}
+    {% set rel_type = api.Relation.get_relation_type(row[3]) %}
+    {% set existing = api.Relation.create(database=rel_db, schema=rel_schema, identifier=rel_identifier, type=rel_type) %}
+    {% do drop_relation(existing) %}
+  {% endfor %}
+  {%- call statement('drop_schema') -%}
+    {% do log('drop_schema', info=true) %}
+    drop schema if exists {{ relation }}
+  {% endcall %}
+{% endmacro %}
+
+
+{% macro trino__rename_relation(from_relation, to_relation) -%}
+  {% do log('trino__rename_relation', info=true) %}
+  {% set from_relation_type = 'materialized view' if from_relation.type == 'materializedview' else from_relation.type %}
+  {% call statement('rename_relation') -%}
+    alter {{ from_relation_type }} {{ from_relation }} rename to {{ to_relation }}
+  {%- endcall %}
+{% endmacro %}
+
+
+{% macro trino__alter_relation_comment(relation, relation_comment) -%}
+  {% do log('trino__alter_relation_comment', info=true) %}
+  comment on {{ relation.type }} {{ relation }} is '{{ relation_comment | replace("'", "''") }}';
+{% endmacro %}
+
+
+{% macro trino__alter_column_comment(relation, column_dict) %}
+  {% set existing_columns = adapter.get_columns_in_relation(relation) | map(attribute="name") | list %}
+  {% for column_name in column_dict if (column_name in existing_columns) %}
+    {% set comment = column_dict[column_name]['description'] %}
+    {%- if comment|length -%}
+      comment on column {{ relation }}.{{ adapter.quote(column_name) if column_dict[column_name]['quote'] else column_name }} is '{{ comment | replace("'", "''") }}';
+    {%- else -%}
+      comment on column {{ relation }}.{{ adapter.quote(column_name) if column_dict[column_name]['quote'] else column_name }} is null;
+    {%- endif -%}
+  {% endfor %}
+{% endmacro %}
+
+
+{% macro trino__list_schemas(database) -%}
+  {% do log('trino__list_schemas', info=true) %}
+  {% do log(information_schema_name(database), info=true) %}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) %}
+    select distinct schema_name
+    from {{ information_schema_name(database) }}.schemata
+    where catalog_name = '{{ database }}'
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
+{% endmacro %}
+
+
+{% macro trino__check_schema_exists(information_schema, schema) -%}
+  {% do log('trino__check_schema_exists', info=true) %}
+  {% call statement('check_schema_exists', fetch_result=True, auto_begin=False) -%}
+        select count(*)
+        from {{ information_schema }}.schemata
+        where catalog_name = '{{ information_schema.database }}'
+          and schema_name = '{{ schema | lower }}'
+  {%- endcall %}
+  {{ return(load_result('check_schema_exists').table) }}
+{% endmacro %}
+
+{% macro trino__get_binding_char() %}
+  {%- if target.prepared_statements_enabled|as_bool -%}
+    {{ return('?') }}
+  {%- else -%}
+    {{ return('%s') }}
+  {%- endif -%}
+{% endmacro %}
+
+
+{% macro trino__alter_relation_add_remove_columns(relation, add_columns, remove_columns) %}
+  {% if add_columns is none %}
+    {% set add_columns = [] %}
+  {% endif %}
+  {% if remove_columns is none %}
+    {% set remove_columns = [] %}
+  {% endif %}
+
+  {% for column in add_columns %}
+    {% set sql -%}
+      alter {{ relation.type }} {{ relation }} add column {{ column.name }} {{ column.data_type }}
+    {%- endset -%}
+    {% do run_query(sql) %}
+  {% endfor %}
+
+  {% for column in remove_columns %}
+    {% set sql -%}
+      alter {{ relation.type }} {{ relation }} drop column {{ column.name }}
+    {%- endset -%}
+    {% do run_query(sql) %}
+  {% endfor %}
+{% endmacro %}
+
+
+{% macro create_or_replace_view() %}
+  {%- set identifier = model['alias'] -%}
+
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+  {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
+
+  {%- set target_relation = api.Relation.create(
+      identifier=identifier, schema=schema, database=database,
+      type='view') -%}
+  {% set grant_config = config.get('grants') %}
+
+  {{ run_hooks(pre_hooks) }}
+
+  -- If there is another object delete it
+  {%- if old_relation is not none and not old_relation.is_view -%}
+    {{ handle_existing_table(should_full_refresh(), old_relation) }}
+  {%- endif -%}
+
+  -- build model
+  {% call statement('main') -%}
+    {{ get_create_view_as_sql(target_relation, sql) }}
+  {%- endcall %}
+
+  {% set should_revoke = should_revoke(exists_as_view, full_refresh_mode=True) %}
+  {% do apply_grants(target_relation, grant_config, should_revoke=True) %}
+
+  {{ run_hooks(post_hooks) }}
+
+  {{ return({'relations': [target_relation]}) }}
+{% endmacro %}
